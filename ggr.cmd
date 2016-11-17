dc22dd513c1698cd49e619de632f447f48207bdc
Jason Du <dujingbobox@gmail.com>
2016-11-17 13:55:22 +0800

    Create ggr.cmd

---
+++ ggr.cmd
@@ -0,0 +1,8 @@
+git config --global core.
+
+git config --global user.email jasondu@hotmail.com.au
+git config --global user.name "Jason Du"
+git config --global core.autocrlf input
+git config --global core.eol lf
+rem core.excludesfile=/c/ggi.txt
+git config --global core.filemode false
